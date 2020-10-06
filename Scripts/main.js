7b2a20ceaeaa5b7bc065d358355a1193a405bf42
Paul J. Davis <paul.joseph.davis@gmail.com>
2020-10-06 16:10:58 -0500

    Attempting to add Go To Deifnition

--- Scripts/main.js
+++ Scripts/main.js
@@ -15,6 +15,76 @@ exports.deactivate = function() {
     }
 };
 
+const compositeDisposable = new CompositeDisposable();
+
+
+function registerGoToDefinition(client) {
+    function toLSPRange(document, range) {
+        const contents = document.getTextInRange(new Range(0, document.length));
+        let chars = 0;
+        let range_start = undefined;
+        const lines = contents.split(document.eol);
+        for(let idx = 0; idx < lines.length; idx++) {
+            const line_len = lines[idx].length + document.eol.length;
+            if(!range_start && chars + line_len >= range.start) {
+                range_start = {
+                    line: idx,
+                    character: range.start - chars
+                };
+            }
+            if(range_start && chars + line_len >= range.end) {
+                return {
+                    start: range_start,
+                    end: {
+                        line: idx,
+                        character: range.end - chars
+                    }
+                };
+            }
+            chars += line_len;
+          }
+          return null;
+    }
+
+    function goToDefinition(editor) {
+        console.log("Going to definition");
+        try {
+            const selectedRange = editor.selectedRange;
+            const selectedPosition = toLSPRange(editor.document, selectedRange).start;
+            if (!selectedPosition) {
+                nova.workspace.showWarningMessage(
+                    "Couldn't figure out what you've selected."
+                );
+                return;
+            }
+            const definitionParams = {
+                textDocument: {uri: editor.document.uri},
+                position: selectedPosition
+            };
+            const response = client.sendRequest(
+                "textDocument/definition",
+                definitionParams
+            );
+            if(response == null) {
+                nova.workspace.showWarningMessage("Couldn't find definition.");
+                return;
+            }
+    
+            console.log(JSON.stringify(response));
+    
+            nova.workspace.showWarningMessage("Yay command!");
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    console.log("Registering goToDefinition");
+    return nova.commands.register(
+        "erlang.goToDefinition",
+        goToDefinition
+    );
+}
+
 class ErlangLanguageServer {    
     constructor() {        
         nova.workspace.onDidAddTextEditor((editor) => {
@@ -66,6 +136,22 @@ class ErlangLanguageServer {
             ]
         };
         var client = new LanguageClient("erlang", "Erlang Language Server", serverOptions, clientOptions);
+        
+        try {
+            compositeDisposable.add(registerGoToDefinition(client));
+            
+            client.onNotification("initialized", () => {
+                console.log("initialized");
+            });
+            
+            client.onNotification("window/showMessage", (params) => {
+                console.log("window/showMessage", JSON.stringify(params));
+            });
+        } catch(err) {
+            console.log(err);
+        }
+        
+        console.log("Staring Erlang Language Server Client");
         
         try {
             // Start the client
